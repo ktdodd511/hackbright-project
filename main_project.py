83e53f1b55c18a2d49e5e629206d89a1d12232c0
ktdodd511 <katiedodd@Katies-MacBook-Air-2.local>
2017-02-10 11:59:06 -0800

    fixed names of files

--- main_project.py
+++ main_project.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2.7
 
-import test_new_triva_game, timer_test, states_game
+import triva_game, math_game, states_game
 
 
 def main():
@@ -8,9 +8,9 @@ def main():
         print "\nHello there, lover of games!\nPlease pick from the following menu:\n"
         pick_game = raw_input("\nPress 1 for Trivia\nPress 2 for Fun with Numbers\nPress 3 for States Game\nPress 'Q' to quit at anytime.\n")
         if pick_game == '1':
-            test_new_triva_game.main()
+            triva_game.main()
         elif pick_game == '2':
-            timer_test.main()
+            math_game.main()
         elif pick_game == '3':
             states_game.main()
         elif pick_game == 'q':
